ea1c1f42dd98fa9e8df99469e3e2e42562acbf62
SonicYeager <kimstone.yeager@gmail.com>
2021-09-20 14:52:56 +0200

    declaring interfaces and structs;

--- TraceLogParserLogic/Contracts.cs
+++ TraceLogParserLogic/Contracts.cs
@@ -1,19 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace tracelogparserlogic
 {
-    public interface ICMDParser
+    public interface ICommandParser
     {
-
+        public ParseCommandData ParseCLIArgs(string args); //throw??
     }
 
     public interface ITraceLogParser
     {
-
+        //public CSVData ParseTraceLog(TraceLogData);
     }
 
     public interface ICSVFileWriter
     {
+        public void ReadTextFile(CSVFile file);
+    }
 
+    public interface ITextFileReader
+    {
+        public TraceLogFile ReadTextFile(string path);
+    }
+
+    public interface ICLIUI
+    {
+        public void Print(string msg);
+    }
+
+    public interface IMainController
+    {
+        public void Run(List<string> args);
+    }
+
+    public interface ITraceLogParseController
+    {
+        public void ParseTraceLogToCSV(string dst, List<string> srcFiles);
     }
 }
--- TraceLogParserLogic/InternalTypes.cs
+++ TraceLogParserLogic/InternalTypes.cs
@@ -14,5 +14,13 @@ namespace tracelogparserlogic
         public List<string> Headers { get; set; }
         public List<List<string>> Elements { get; set; }
         public string Seperator { get; set; }
+        public string FilePath { get; set; }
     }
+
+    public struct ParseCommandData
+    {
+        public string DestinationPath { get; set; }
+        public List<string> SourceFilePaths { get; set; }
+    }
+
 }
