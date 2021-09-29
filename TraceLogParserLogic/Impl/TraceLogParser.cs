2d778bfba22429f90d2f637259d83ddfde875e64
SonicYeager <kimstone.yeager@gmail.com>
2021-09-29 17:27:53 +0200

    Prept regex;

--- TraceLogParserLogic/Impl/TraceLogParser.cs
+++ TraceLogParserLogic/Impl/TraceLogParser.cs
@@ -16,21 +16,80 @@ namespace tracelogparserlogic
     //fetch data block                             (?<IsDataLine>([[]{1}INFO[]]{1}\s+[[]{1}VideoEngine[]]{1})?(\d\d:\d\d:\d\d)\s(\d+.\d+$))
     //sperate data block                           (?<Frame>\d\d:\d\d:\d\d)|(?<Duration>(\d+.\d+$))
     //parse framecount                             (?<Minute>\d\d):(?<Second>\d\d):(?<Frames>\d\d)
+
     public class TraceLogParser : ITraceLogParser
     {
         public event OnParsed onParsed;
 
-        void ParseTraceLogFrameTimeOnly(TraceLogFile traceLogFile, string dstPath)
+        bool IsStartFrameTimeLine(string line)
         {
-            List<List<string>> parsedElements = new();
+            Regex regex = new("(?<IsStart>[[]{1}INFO[]]{1}\\s+[[]{1}PerformanceMeasurement[]]{1}:\\sStart\\s\\w*\\s[[]\\d[]]\\s[|]\\sFrame:)");
+            Match match = regex.Match(line);
+            return match.Success;
+        }
 
-            var newPath = Path.GetFullPath(dstPath) + Path.GetFileNameWithoutExtension(Path.GetFullPath(traceLogFile.FilePath)) + ".csv";
+        bool IsStopFrameTimeLine(string line)
+        {
+            Regex regex = new("(?<IsStop>[[]{1}INFO[]]{1}\\s+[[]{1}PerformanceMeasurement[]]{1}:\\sStop\\s\\w*\\s[[]\\d[]]->\\s\\w*\\s\\d.\\d*\\sms\\s[|]\\sFrame:)");
+            Match match = regex.Match(line);
+            return match.Success;
+        }
+
+        bool IsStartMethodLine(string line)
+        {
+            Regex regex = new("(?<IsStart>[[]{1}INFO[]]{1}\\s+[[]{1}PerformanceMeasurement[]]{1}:\\sStart\\s\\w*\\s[[]\\d*[]])");
+            Match match = regex.Match(line);
+            return match.Success;
+        }
+
+        bool IsStopMethodTimeLine(string line)
+        {
+            Regex regex = new("(?<IsStart>[[]{1}INFO[]]{1}\\s+[[]{1}PerformanceMeasurement[]]{1}:\\sStop\\s\\w*\\s[[]\\d*[]])");
+            Match match = regex.Match(line);
+            return match.Success;
+        }
+
+        bool HasFrameCount(string line)
+        {
+            Regex regex = new("(?<Frame>\\d\\d:\\d\\d:\\d\\d$)");
+            Match match = regex.Match(line);
+            return match.Success;
+        }
+
+        bool HasFrameTime(string line)
+        {
+            Regex regex = new("(?<Duration>(\\d+.\\d+(?=\\sms)))");
+            Match match = regex.Match(line);
+            return match.Success;
+        }
+
+        bool HasDuration(string line)
+        {
+            Regex regex = new("(?<Duration>(\\d+.\\d+(?=\\sms)))");
+            Match match = regex.Match(line);
+            return match.Success;
+        }
+
+        bool HasMethodName(string line)
+        {
+            Regex regex = new("(?<MethodName>(\\w*(?=\\s[[]\\d*[]])))"); //TODO
+            Match match = regex.Match(line);
+            return match.Success;
+        }
+
+        public void ParseTraceLog(TraceLogFile traceLogFile, string dstPath)
+        {
+            List<List<string>> parsedFrameTime = new();
+            List<List<string>> parsedRunTime = new();
+
+            var newFTPath = Path.GetFullPath(dstPath) + Path.GetFileNameWithoutExtension(Path.GetFullPath(traceLogFile.FilePath)) + "_FT" + ".csv";
+            var newRTPath = Path.GetFullPath(dstPath) + Path.GetFileNameWithoutExtension(Path.GetFullPath(traceLogFile.FilePath)) + "_RT" + ".csv";
 
             foreach (string line in traceLogFile.Lines)
             {
                 List<string> results = new();
 
-                Regex regex = new("(?<IsDataLine>([[]{1}INFO[]]{1}\\s+[[]{1}VideoEngine[]]{1})?(?<Data>\\d\\d:\\d\\d:\\d\\d)\\s(\\d+.\\d+$))");
+                Regex regex = new("(?<IsStartFrameTimeLine>([[]{1}INFO[]]{1}\\s+[[]{1}PerformanceMeasurement[]]{1}:)?(?<Data>\\d\\d:\\d\\d:\\d\\d)\\s(\\d+.\\d+$))");
                 Match match = regex.Match(line);
                 if (match.Success)
                 {
@@ -53,25 +112,12 @@ namespace tracelogparserlogic
                         groups.TryGetValue("Duration", out group);
                         results.Add(group.Value);
                     }
-                    parsedElements.Add(results);
+                    parsedFrameTime.Add(results);
                 }
             }
 
-            onParsed.Invoke(new CSVFile() { Seperator = ';', FilePath = newPath, Headers = new List<string>() { "Frame", "Duration" }, Elements = parsedElements }); //frameTime
-        }
-
-        void ParseTraceLogFrameAndRunTime(TraceLogFile traceLogFile, string dstPath)
-        {
-            List<List<string>> parsedFrameTimes = new();
-            List<List<string>> parsedRunTimes = new();
-
-            var newPath = Path.GetFullPath(dstPath) + Path.GetFileNameWithoutExtension(Path.GetFullPath(traceLogFile.FilePath)) + ".csv";
-        }
-
-        public void ParseTraceLog(TraceLogFile traceLogFile, string dstPath)
-        {
-            //dtermine style of logfile
-            //start corresponding algo
+            onParsed.Invoke(new CSVFile() { Seperator = ';', FilePath = newFTPath, Headers = new List<string>() { GlobalConstants.FrameHeaderText, GlobalConstants.DurationHeaderText }, Elements = parsedFrameTime }); //frameTime
+            onParsed.Invoke(new CSVFile() { Seperator = ';', FilePath = newRTPath, Headers = new List<string>() { GlobalConstants.FrameHeaderText, GlobalConstants.MethodNameHeaderText, GlobalConstants.RunTimeHeaderText }, Elements = parsedRunTime }); //frameTime
         }
     }
 }
