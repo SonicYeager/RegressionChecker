1f0353620f53e3059a1ffbb72c2c277455d564e8
Magnus Cook <69338558+SonicYeager@users.noreply.github.com>
2021-10-18 12:51:00 +0200

    implemented algo; fixing bugs according to unittests;

--- UnitTests/RegressionEvaluator_UnitTests.cs
+++ UnitTests/RegressionEvaluator_UnitTests.cs
@@ -47,7 +47,7 @@ namespace UnitTests
             };
             CSVFile expected = new CSVFile()
             {
-                FilePath = "C:\\msvc\\TestLog001_FT_RL.csv",
+                FilePath = "D:\\Temp\\RL_0.csv",
                 Headers = new List<string>() { "Frame", "MethodName", "RunTime" },
                 Elements = new List<List<string>>() {
                 }
@@ -56,7 +56,7 @@ namespace UnitTests
             var result = eval.EvaluateRegression(
                 new ReferenceData() { FrameTimes=new List<CSVFile>() { refFrameTimes }, MethodRunTimesPerFrame=new List<CSVFile>() { refRunTimes } },
                 new LatestData() { FrameTimes=frameTimes, MethodRunTimesPerFrame=runTimes },
-                "D:\\Temp");
+                "D:\\Temp\\");
             
             Assert.Equal(expected, result);
         }
@@ -101,7 +101,7 @@ namespace UnitTests
             };
             CSVFile expected = new CSVFile()
             {
-                FilePath = "C:\\msvc\\TestLog001_FTRL0.csv",
+                FilePath = "D:\\Temp\\RL_0.csv",
                 Headers = new List<string>() { "Frame", "MethodName", "RunTime" },
                 Elements = new List<List<string>>()
                 {
@@ -112,7 +112,7 @@ namespace UnitTests
             var result = eval.EvaluateRegression(
                 new ReferenceData() { FrameTimes = new List<CSVFile>() { refFrameTimes }, MethodRunTimesPerFrame = new List<CSVFile>() { refRunTimes } },
                 new LatestData() { FrameTimes = frameTimes, MethodRunTimesPerFrame = runTimes },
-                "D:\\Temp");
+                "D:\\Temp\\");
 
             Assert.Equal(expected, result);
         }
@@ -169,7 +169,7 @@ namespace UnitTests
             };
             CSVFile expected = new CSVFile()
             {
-                FilePath = "C:\\msvc\\TestLog001_FTRL0.csv",
+                FilePath = "D:\\Temp\\RL_0.csv",
                 Headers = new List<string>() { "Frame", "MethodName", "RunTime" },
                 Elements = new List<List<string>>()
                 {
@@ -236,7 +236,7 @@ namespace UnitTests
             };
             CSVFile expected = new CSVFile()
             {
-                FilePath = "C:\\msvc\\TestLog001_FTRL0.csv",
+                FilePath = "D:\\Temp\\RL_0.csv",
                 Headers = new List<string>() { "Frame", "MethodName", "RunTime" },
                 Elements = new List<List<string>>()
                 {
--- regressionevallogic/Impl/RegressionEvaluator.cs
+++ regressionevallogic/Impl/RegressionEvaluator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -26,28 +27,40 @@ namespace regressionevallogic
             {
                 double sum = 0;
                 foreach (var frameTime in frameTimes)
-                    sum += Convert.ToDouble(frameTime.Elements[i][1]);
+                    sum += Convert.ToDouble(frameTime.Elements[i][1], new NumberFormatInfo() { NumberDecimalSeparator = "." });
                 averaged.Add((sum / (double)frameTimes.Count));
             }
 
             for (int i = 0; i < smallestIndex; ++i)
-                averagedFrameTimes.Elements.Add(new List<string>() { frameTimes[0].Elements[i][0], string.Format("{0:N4}",Convert.ToString(averaged[i]))});
+                averagedFrameTimes.Elements.Add(new List<string>() { frameTimes[0].Elements[i][0], string.Format("{0:N4}",Convert.ToString(averaged[i], new NumberFormatInfo() { NumberDecimalSeparator = "." }))});
 
             return averagedFrameTimes;
             
         }
         public CSVFile EvaluateRegression(ReferenceData refData, LatestData latestData, string path)
         {
-            var newPath = Path.GetFullPath(path) + Path.GetFileNameWithoutExtension(Path.GetFullPath(latestData.FrameTimes.FilePath)) + "RL_" + _count + ".csv";
+            var newPath = Path.GetFullPath(path) + "RL_" + _count + ".csv";
             ++_count;
 
-            CSVFile evaluated = new() { FilePath = "", Seperator = '\0', Elements = new List<List<string>>(), Headers = new List<string>() { "Frame", "MethodName", "RunTime" } }; //objektname?
+            CSVFile evaluated = new() { FilePath = newPath, Seperator = '\0', Elements = new List<List<string>>(), Headers = new List<string>() { "Frame", "MethodName", "RunTime" } }; //objektname?
             //get mittelwert from refData
             var averaged = GetAverageFrameTimes(refData.FrameTimes);
             //check for lenght -> use smallest
-            //iterate through frametimes
-            //compare frametimes
-            //when frametime greater than ref -> Mark the frame index
+            int smallestIndex = Math.Min(averaged.Elements.Count, latestData.FrameTimes.Elements.Count);
+            //iterate through frametimes + compare frametimes
+            for (int i = 0; i < smallestIndex; ++i)
+            {
+                //when frametime greater than ref -> Mark the frame index
+                if (Convert.ToDouble(averaged.Elements[i][1], new NumberFormatInfo() { NumberDecimalSeparator = "." }) < Convert.ToDouble(latestData.FrameTimes.Elements[i][1], new NumberFormatInfo() { NumberDecimalSeparator = "." }))
+                {
+                    string frame = latestData.FrameTimes.Elements[i][0];
+                    evaluated.Elements.Add(new List<string>() {
+                        latestData.FrameTimes.Elements[i][0],
+                        latestData.MethodRunTimesPerFrame.Elements.Find((line) => line[0] == frame)[1],
+                        latestData.MethodRunTimesPerFrame.Elements.Find((line) => line[0] == frame)[2],
+                    });
+                }
+            }
             //return this: compile list with frame index and longest runtime method within that frame
             return evaluated;
         }
