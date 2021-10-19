d1cd29435282d834adc3222147bc572a74408526
Magnus Cook <69338558+SonicYeager@users.noreply.github.com>
2021-10-19 15:50:25 +0200

    refac part 1;

--- TraceLogParserLogic/Impl/TraceLogParser.cs
+++ TraceLogParserLogic/Impl/TraceLogParser.cs
@@ -4,91 +4,122 @@ using System.Text.RegularExpressions;
 
 namespace tracelogparserlogic
 {
-    //FrameFilter                                  (?<Frame>\d\d:\d\d:\d\d)
-    //[INFO]\t[VideoEngine]Filter                  (?<IsDataLine>([[]{1}INFO[]]{1}\s+[[]{1}VideoEngine[]]{1}))
-    //[INFO]\t[PerformanceMeasurement]Filter
-    //DurationFilter                               (?<Duration>(\d+.\d+$))
-
-    //fetch data block                             (?<IsDataLine>([[]{1}INFO[]]{1}\s+[[]{1}VideoEngine[]]{1})?(\d\d:\d\d:\d\d)\s(\d+.\d+$))
-    //sperate data block                           (?<Frame>\d\d:\d\d:\d\d)|(?<Duration>(\d+.\d+$))
-    //parse framecount                             (?<Minute>\d\d):(?<Second>\d\d):(?<Frames>\d\d)
-
     public class TraceLogParser : ITraceLogParser
     {
         public event OnParsed onParsed;
 
+        ///{ CONSTANTS 
+        ///REGEXGROUPNAME
+        private static readonly string GRN_FRAME = "Frame";
+        private static readonly string GRN_FRAMETIME = "FrameTime";
+        private static readonly string GRN_DURATION = "Duration";
+        private static readonly string GRN_METHODNAME = "MethodName";
+        ///REGEX
+        private static readonly string STARTFRAMETIME = "(?<IsStart>[[]{1}INFO[]]{1}\\s+[[]{1}PerformanceMeasurement[]]{1}:\\sStart\\s\\w*\\s[[]\\d*[]]\\s[|]\\sFrame:)";
+        private static readonly string STOPFRAMETIME = "(?<IsStop>[[]{1}INFO[]]{1}\\s+[[]{1}PerformanceMeasurement[]]{1}:\\sStop\\s\\w*\\s[[]\\d*[]]->\\s\\w*\\s\\d.\\d*\\sms\\s[|]\\sFrame:)";
+        private static readonly string STARTMETHOD = "(?<IsStart>[[]{1}INFO[]]{1}\\s+[[]{1}PerformanceMeasurement[]]{1}:\\sStart\\s\\w*\\s[[]\\d*[]])";
+        private static readonly string STOPMETHOD = "(?<IsStart>[[]{1}INFO[]]{1}\\s+[[]{1}PerformanceMeasurement[]]{1}:\\sStop\\s\\w*\\s[[]\\d*[]])";
+        private static readonly string FRAMECOUNT = "(?<" + GRN_FRAME + ">\\d\\d:\\d\\d:\\d\\d$)";
+        private static readonly string FRAMETIME = "(?<" + GRN_FRAMETIME + ">(\\d+.\\d+(?=\\sms))|(\\d+(?=\\sms)))";
+        private static readonly string DURATION = "(?<" + GRN_DURATION + ">(\\d+.\\d+(?=\\sms))|(\\d+(?=\\sms)))";
+        private static readonly string METHODNAME = "(?<" + GRN_METHODNAME + ">(\\w*(?=\\s[[]\\d*[]])))";
+        ///}
+
         bool IsStartFrameTimeLine(string line)
         {
-            Regex regex = new("(?<IsStart>[[]{1}INFO[]]{1}\\s+[[]{1}PerformanceMeasurement[]]{1}:\\sStart\\s\\w*\\s[[]\\d*[]]\\s[|]\\sFrame:)");
+            Regex regex = new(STARTFRAMETIME);
             Match match = regex.Match(line);
             return match.Success;
         }
 
         bool IsStopFrameTimeLine(string line)
         {
-            Regex regex = new("(?<IsStop>[[]{1}INFO[]]{1}\\s+[[]{1}PerformanceMeasurement[]]{1}:\\sStop\\s\\w*\\s[[]\\d*[]]->\\s\\w*\\s\\d.\\d*\\sms\\s[|]\\sFrame:)");
+            Regex regex = new(STOPFRAMETIME);
             Match match = regex.Match(line);
             return match.Success;
         }
 
         bool IsStartMethodLine(string line)
         {
-            Regex regex = new("(?<IsStart>[[]{1}INFO[]]{1}\\s+[[]{1}PerformanceMeasurement[]]{1}:\\sStart\\s\\w*\\s[[]\\d*[]])");
+            Regex regex = new(STARTMETHOD);
             Match match = regex.Match(line);
             return match.Success;
         }
 
         bool IsStopMethodTimeLine(string line)
         {
-            Regex regex = new("(?<IsStart>[[]{1}INFO[]]{1}\\s+[[]{1}PerformanceMeasurement[]]{1}:\\sStop\\s\\w*\\s[[]\\d*[]])");
+            Regex regex = new(STOPMETHOD);
             Match match = regex.Match(line);
             return match.Success;
         }
 
         string GetFrameCount(string line)
         {
-            Regex regex = new("(?<Frame>\\d\\d:\\d\\d:\\d\\d$)");
+            Regex regex = new(FRAMECOUNT);
             Match match = regex.Match(line);
             Group val;
-            match.Groups.TryGetValue("Frame", out val);
+            match.Groups.TryGetValue(GRN_FRAME, out val);
             return val.Value;
         }
 
         string GetFrameTime(string line)
         {
-            Regex regex = new("(?<FrameTime>(\\d+.\\d+(?=\\sms))|(\\d+(?=\\sms)))");
+            Regex regex = new(FRAMETIME);
             Match match = regex.Match(line);
             Group val;
-            match.Groups.TryGetValue("FrameTime", out val);
+            match.Groups.TryGetValue(GRN_FRAMETIME, out val);
             return val.Value;
         }
 
         string GetDuration(string line)
         {
-            Regex regex = new("(?<Duration>(\\d+.\\d+(?=\\sms))|(\\d+(?=\\sms)))");
+            Regex regex = new(DURATION);
             Match match = regex.Match(line);
             Group val;
-            match.Groups.TryGetValue("Duration", out val);
+            match.Groups.TryGetValue(GRN_DURATION, out val);
             return val.Value;
         }
 
         string GetMethodName(string line)
         {
-            Regex regex = new("(?<MethodName>(\\w*(?=\\s[[]\\d*[]])))");
+            Regex regex = new(METHODNAME);
             Match match = regex.Match(line);
             Group val;
-            match.Groups.TryGetValue("MethodName", out val);
+            match.Groups.TryGetValue(GRN_METHODNAME, out val);
             return val.Value;
+        }
+
+        string GenerateCSVPathWithMarker(string dstPath, string trclFilePath, string marker)
+        {
+            return Path.GetFullPath(dstPath) + "\\" + Path.GetFileNameWithoutExtension(Path.GetFullPath(trclFilePath)) + marker + ".csv";
+        }
+
+        void ExtractRunTime(string line, string frameCount, string name, string dur, ref List<string> runTimeRes, ref List<List<string>> parsedRunTime)
+        {
+            runTimeRes.Add(frameCount);
+            runTimeRes.Add(name);
+            runTimeRes.Add(dur);
+
+            parsedRunTime.Add(runTimeRes);
+        }
+        void ExtractFrameTime(string line, ref string frameCount, ref List<string> frameTimeRes, ref List<List<string>> parsedFrameTime)
+        {
+            var dur = GetFrameTime(line);
+
+            frameTimeRes.Add(frameCount);
+            frameTimeRes.Add(dur);
+
+            parsedFrameTime.Add(frameTimeRes);
+
+            frameCount = "";
         }
 
         public void ParseTraceLog(TraceLogFile traceLogFile, string dstPath)
         {
             List<List<string>> parsedFrameTime = new();
             List<List<string>> parsedRunTime = new();
-
-            var newFTPath = Path.GetFullPath(dstPath) + "\\" + Path.GetFileNameWithoutExtension(Path.GetFullPath(traceLogFile.FilePath)) + "_FT" + ".csv";
-            var newRTPath = Path.GetFullPath(dstPath) + "\\" + Path.GetFileNameWithoutExtension(Path.GetFullPath(traceLogFile.FilePath)) + "_RT" + ".csv";
-
+            var newFTPath = GenerateCSVPathWithMarker(dstPath, traceLogFile.FilePath, "_FT");
+            var newRTPath = GenerateCSVPathWithMarker(dstPath, traceLogFile.FilePath, "_RT");
             string frameCount = "";
 
             foreach (string line in traceLogFile.Lines)
@@ -102,23 +133,11 @@ namespace tracelogparserlogic
                 {
                     var name = GetMethodName(line);
                     var dur = GetDuration(line);
-
-                    runTimeRes.Add(frameCount);
-                    runTimeRes.Add(name);
-                    runTimeRes.Add(dur);
-
-                    parsedRunTime.Add(runTimeRes);
+                    ExtractRunTime(line, frameCount, name, dur, ref runTimeRes, ref parsedRunTime);
                 }
                 else if (IsStopFrameTimeLine(line))
                 {
-                    var dur = GetFrameTime(line);
-
-                    frameTimeRes.Add(frameCount);
-                    frameTimeRes.Add(dur);
-
-                    parsedFrameTime.Add(frameTimeRes);
-
-                    frameCount = "";
+                    ExtractFrameTime(line, ref frameCount, ref frameTimeRes, ref parsedFrameTime);
                 }
             }
 
